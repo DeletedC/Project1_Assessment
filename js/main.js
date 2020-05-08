ff885831e2f1d4b2a1a2a72fba8f967cfe9a7399
Curtis <CurtisWoodsArt@gmail.com>
2020-05-08 12:43:39 -0400

    Added and botched a garbage check. Commented out

--- js/main.js
+++ js/main.js
@@ -14,7 +14,16 @@ let $inputVal = $('input').val();
 // Functions
 /////////////////////
 
+// I thought a garbage check would be a good idea.
+// Botched it up.
+// const garbageCheck = () => {
+//     if (parseInt($inputVal, 10) !== Number) {
+//         $inputVal = $('input').val(0);
+//     }
+// }
+
 const setInputVal = () => {
+    //garbageCheck(); // This function is not complete
     $inputVal = $('input').val();
     console.log($inputVal);
 }
