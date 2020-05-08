b1cacc3d887b99147d39a19f83c19a108e3119f2
Curtis <CurtisWoodsArt@gmail.com>
2020-05-08 11:19:59 -0400

    Added js variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 console.log("main.js CONNECTED");
+
+///////////////////////
+// Variables
+///////////////////////
+
+const $result = $('#result');
+const $btnPlus = $('#btnPlus');
+const $btnMinus = $('#btnMinus');
+const $input = $('input');
+
+
+$(() => {
+    
+})
